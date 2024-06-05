16fa14802a9079647f108aa8a7d8c7e98ee5f763
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-06-05 23:27:51 +0200

    add PermutationDifference class

--- src/main/java/arraystring/permutationdifference/PermutationDifference.java
+++ src/main/java/arraystring/permutationdifference/PermutationDifference.java
@@ -19,11 +19,11 @@ public class PermutationDifference {
     public static int findPermutationDifference(String s, String t) {
         int absSum = 0;
         Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), i);
-        }
         for (int i = 0; i < t.length(); i++) {
-            absSum += Math.abs(i - map.get(t.charAt(i)));
+            map.put(t.charAt(i), i);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            absSum += Math.abs(i - map.get(s.charAt(i)));
         }
         return absSum;
     }
