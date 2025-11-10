eece376223120ab9dfcc93aeed6d754e035b537d
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2025-11-10 18:08:32 +0100

    add recoverOrder method in RestoreFinishingOrder class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -59,6 +59,7 @@ import arraystring.permutationdifference.PermutationDifference;
 import arraystring.pivotindex.PivotIndex;
 import arraystring.removeduplicates.RemoveDuplicates;
 import arraystring.removeelement.RemoveElement;
+import arraystring.restorefinishingorder.RestoreFinishingOrder;
 import arraystring.reversedegreestring.ReverseDegreeOfString;
 import arraystring.reverseprefixword.ReversePrefixWord;
 import arraystring.reversestring.ReverseString;
@@ -865,6 +866,12 @@ public class Main {
         int[] monums = {3,2};
         System.out.println(MinOperations.minOperations(monums, mokk));
         System.out.println(MinOperations.minOperations2(monums, mokk));
+
+        //String -> RestoreFinishingOrder
+        System.out.println("RestoreFinishingOrder");
+        int[] rfo1 = {3,1,2,5,4};
+        int[] rfo2 = {1,3,4};
+        System.out.println(Arrays.toString(RestoreFinishingOrder.recoverOrder(rfo1, rfo2)));
 
 
 
